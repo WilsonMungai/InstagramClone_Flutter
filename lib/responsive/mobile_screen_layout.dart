0e1103d313af9ec1ba5b226455d541f39e040e18
WilsonMungai <wilsonmuguthi@gmail.com>
2023-03-08 21:04:48 -0500

    added tab bar

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,9 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:isntagram/providers/user_provider.dart';
-import 'package:isntagram/models/user.dart' as model;
-import 'package:provider/provider.dart';
+import 'package:isntagram/utils/colors.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
@@ -13,10 +10,46 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  int _page = 0;
   @override
   Widget build(BuildContext context) {
-    // get user from provider
-    model.User user = Provider.of<UserProvider>(context).getUser;
-    return Scaffold(body: Center(child: Text(user.username)));
+    return Scaffold(
+      body: Center(child: Text('This is a mobile screen layout.')),
+      // bottom navigation bar
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        // tab bar items
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: _page == 0 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.add_circle,
+                color: _page == 1 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+                color: _page == 2 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: _page == 3 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor),
+        ],
+      ),
+    );
   }
 }
